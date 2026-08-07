9760315195bf757a193394c1997d2a69e2d1ee19
Fillo <filloinformatica@gmail.com>
2026-08-08 01:02:07 +0200

    fix: solved bug about domain inconsistency

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://filippobratti.com',
+  site: 'https://www.filippobratti.com',
   i18n: {
     locales: ['it', 'en'],
     defaultLocale: 'it',
