91c928c19fcdf15341225def0758e1db34165161
Fillo <filloinformatica@gmail.com>
2026-08-07 16:30:43 +0200

    feat: added new domain name to the sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://personal-page-eta-six.vercel.app',
+  site: 'https://filippobratti.com',
   integrations: [sitemap()]
 });
